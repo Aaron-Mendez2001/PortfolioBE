53b21d083825742c369e5095d576b19d95c45dd4
Aaron Mendez <ajmendez01@outlook.com>
2021-07-30 21:05:47 -0700

    Created skills and experience table for dev and test db

---
+++ data/migrations/20210731034614_create-tables.js
@@ -0,0 +1,27 @@
+
+exports.up = async function(knex) {
+    await knex.schema.createTable('skills', (table) => {
+        table.increments('id')
+        table.text('name')
+        table.text('education')
+        table.text('description')
+    })
+
+    await knex.schema.createTable('experience', (table) => {
+        table.increments('id')
+        table.text('name')
+        table.text('url')
+        table.text('description')
+        table.text('skills_1')
+            .references('id')
+            .inTable('skills')
+            .onDelete('SET NULL')
+            .onUpdate('CASCADE')
+
+  })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('skills')
+    await knex.schema.dropTableIfExists('experience')
+};
