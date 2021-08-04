a12baefcc171d5ad369b6075d5daabaec61e4423
Aaron Mendez <ajmendez01@outlook.com>
2021-08-04 13:45:19 -0700

    Added the homeRouter to the server file and the dotenv config

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const homeRouter = require('./home/home-router');
 const skillsRouter = require('./skills/skills-router');
 const expRouter = require('./experience/experience-router');
+
+require('dotenv').config();
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet);
 server.use(cors);
+server.use(homeRouter);
 server.use('/skills', skillsRouter)
 server.use('/experience', expRouter);
 
