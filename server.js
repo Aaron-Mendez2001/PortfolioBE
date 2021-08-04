900b9ff9c4f55ce5cf8167b143b533207a5d6c70
Aaron Mendez <ajmendez01@outlook.com>
2021-08-04 14:16:59 -0700

    Made a correction in server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use(cors);
+server.use(cors());
 server.use(homeRouter);
 server.use('/skills', skillsRouter)
 server.use('/experience', expRouter);
