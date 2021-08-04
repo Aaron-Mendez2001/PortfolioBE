83a74b4cb338b779f02c4b7f4b629f69beb98fa2
Aaron Mendez <ajmendez01@outlook.com>
2021-08-04 13:40:53 -0700

    Added the skills and exp routers to the server file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const skillsRouter = require('./skills/skills-router');
+const expRouter = require('./experience/experience-router');
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet);
 server.use(cors);
+server.use('/skills', skillsRouter)
+server.use('/experience', expRouter);
 
 server.use((err, req, res, next) => {
     console.log(err);
